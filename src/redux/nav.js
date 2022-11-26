d0205f4a3c7cef5ae40721a192de7aacae732ca6
Abhiram Sankar <abhiramsankar102000@gmail.com>
2022-11-26 21:27:42 +0300

    Loader responsive

--- src/redux/nav.js
+++ src/redux/nav.js
@@ -7,7 +7,7 @@ export const navigation = createSlice ({
         isLoading: false,
         isReload: true,
         sectionCount: 1,
-        maintenance: true,
+        maintenance: false  ,
     },
     reducers: {
         setCurrent: (state, action) => {
